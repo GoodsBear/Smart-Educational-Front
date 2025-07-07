6b92a6acb1b29856e890f223a5e3069f096616ad
小困鱼0329 <2998198261@qq.com>
2025-07-07 15:32:16 +0800

    1

---
+++ Educational-Front-develop/src/components/RichTextDisplay/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ plainText }}</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { stripHtmlTags } from '@/utils';
+
+const props = defineProps({
+  content: {
+    type: String,
+    default: '',
+  },
+});
+
+const plainText = computed(() => stripHtmlTags(props.content));
+</script>
